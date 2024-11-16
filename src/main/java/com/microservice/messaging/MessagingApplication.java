d001facab2fa686739e32e1b084e1e9d43d5c71d
Jose Mokeni <jmmokeni@gmail.com>
2024-11-16 21:09:49 +0100

    config from server ok

--- src/main/java/com/microservice/messaging/MessagingApplication.java
+++ src/main/java/com/microservice/messaging/MessagingApplication.java
@@ -2,8 +2,10 @@ package com.microservice.messaging;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
+@RefreshScope
 public class MessagingApplication {
 
 	public static void main(String[] args) {
